0bdee6b6e7f0523884fed61d4a1a5f3285ea99f4
bcbane <bc.bane4@gmail.com>
2016-05-09 14:55:29 -0600

    Update myFirstCodeFile.js

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -4,10 +4,12 @@ var flyingF = 67;
 
 var yellingChimp = "99 red balloons"
 
+var fish = "zebra shark"
+
 var journal = [];
 
 journal.push(dm11);
 journal.push(flyingF);
 journal.push(yellingChimp);
-
+journal.push(fish);
 
